b2c89123bb2e0b6b0dd8888b7e423ef9dd26a812
WindWardPen <siggevarn380@gmail.com>
2026-07-30 23:17:18 +0200

    Added silly test scene and script
    To test this fancy desktop application.

---
+++ scripts/test_script.gd
@@ -0,0 +1,10 @@
+extends Node
+
+const bedtime = 21.00
+var current_time = 23.14
+
+func _ready() -> void:
+	if current_time > bedtime:
+		print("Go to bed for fucks sake")
+		get_tree().quit()
+# This took time to code i could have spent sleeping
