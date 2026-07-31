a8ced769560b4ed43f03070f7dac40216d9810a7
nserafini <nserafini@frba.utn.edu.ar>
2026-07-31 02:57:54 -0300

    Initial set up
    Scripted basic player movement, drone behaviour and dragging mechanic

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Mini-jam-26-07---26-08
 Team consists of: (Discord names)
+legolas1550
--- scripts/test_script.gd
+++ scripts/test_script.gd
@@ -6,5 +6,6 @@ var current_time = 23.14
 func _ready() -> void:
 	if current_time > bedtime:
 		print("Go to bed for fucks sake")
-		get_tree().quit()
+		#get_tree().quit()
 # This took time to code i could have spent sleeping
+	$Drone.toggle_powers.connect($Player.toggle_using_powers)
